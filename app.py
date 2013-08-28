bee205a877c4ec34ae69b6887382cdec00532f62
Stephan Eberle <stephan@deltaciy.org>
2013-08-28 20:09:14 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+
+# Init
+
+app = Flask(__name__)
+
+app.config.update(
+	DEBUG = True,
+)
+
+
+# Controllers
+
+@app.route("/")
+def hello():
+	return "Hello from bvsPI"
+
+
+# Launch service
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+
+
